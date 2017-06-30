f23d7aeebf1b214bc79b45801cf8600b00bb7592
mavijose <ogoumavi@gmail.com>
2017-06-30 09:11:52 +0000

    html.01

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){<font></font>
+  $('.slider1').bxSlider({<font></font>
+    slideWidth: 200,<font></font>
+    minSlides: 2,<font></font>
+    maxSlides: 3,<font></font>
+    slideMargin: 10<font></font>
+  });<font></font>
+});<font></font>
+
+$('.bxslider').bxSlider({
+  minSlides: 7,
+  maxSlides: 8,
+  slideWidth: 300,
+  slideMargin: 1
+});
+
+$(document).ready(function(){
+  $('.slider5').bxSlider({
+    slideWidth: 900,
+    minSlides: 3,
+    maxSlides: 3,
+     startSlide: 2,
+    slideMargin: 6
+  });
+});
